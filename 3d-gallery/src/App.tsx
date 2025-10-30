299993151afb5f5fd8ac8f4779d5d3ea5bbf81c5
Brea Miller <brea.m.miller@gmail.com>
2025-10-30 21:18:47 +0000

    commit

--- 3d-gallery/src/App.tsx
+++ 3d-gallery/src/App.tsx
@@ -2,9 +2,6 @@ import React, { Suspense, useState, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Physics } from '@react-three/cannon'
 import Gallery from './components/Gallery'
-import Lighting from './components/Lighting'
-import Room from './components/Room'
-import FirstPersonControls from './components/FirstPersonControls'
 import LoadingScreen from './components/ui/LoadingScreen'
 import Instructions from './components/ui/Instructions'
 import './App.css'
@@ -95,10 +92,7 @@ function App() {
           >
             <Suspense fallback={null}>
               <Physics gravity={[0, -9.82, 0]} maxSubSteps={5} stepSize={1 / 60}>
-                <Lighting />
-                <Room />
                 <Gallery />
-                <FirstPersonControls />
               </Physics>
             </Suspense>
           </Canvas>
