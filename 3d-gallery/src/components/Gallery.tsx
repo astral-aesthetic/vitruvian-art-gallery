848c31ca13c25de5df9ec88c51247e9acee1cee4
Brea Miller <brea.m.miller@gmail.com>
2025-10-31 01:49:49 +0000

    Re-enable Paintings component

--- 3d-gallery/src/components/Gallery.tsx
+++ 3d-gallery/src/components/Gallery.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import Lighting from './Lighting'
 import Room from './Room'
+import Paintings from './Paintings'
 import FirstPersonControls from './FirstPersonControls'
 
 const Gallery: React.FC = () => {
   try {
-    console.log('[Gallery] Rendering with Room and FirstPersonControls enabled')
+    console.log('[Gallery] Rendering with Room, Paintings, and FirstPersonControls enabled')
     return (
       <>
         <Lighting />
         <Room />
+        <Paintings />
         <FirstPersonControls />
         {/* DISABLED COMPONENTS - To be re-enabled:
           <Sky />
-          <Paintings />
         */}
       </>
     )
