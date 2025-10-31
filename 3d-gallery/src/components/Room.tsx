19019a84e8992d4d2f2c4ac6c52f7069c4faff91
Brea Miller <brea.m.miller@gmail.com>
2025-10-31 01:57:47 +0000

    Fix Room component: remove texture loading and nested mesh

--- 3d-gallery/src/components/Room.tsx
+++ 3d-gallery/src/components/Room.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
-import { useTexture } from '@react-three/drei'
-import { asset } from '@/lib/utils'
 import * as THREE from 'three'
 
 const Room: React.FC = () => {
-  const floorTexture = useTexture(asset('textures/dark_walnut_wood_floor_seamless_texture_3d.jpg'))
-  
-  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping
-  floorTexture.repeat.set(8, 6)
-
   return (
     <group>
-      <mesh receiveShadow position={[0, 0, 0]}>
+      {/* Floor */}
+      <mesh receiveShadow position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
         <planeGeometry args={[12, 12]} />
         <meshStandardMaterial 
-          map={floorTexture} 
+          color="#8B6F47"
           roughness={0.3}
           metalness={0.1}
         />
-        <mesh position={[0, 0, 0.001]} rotation={[-Math.PI / 2, 0, 0]} />
       </mesh>
       
+      {/* Back wall */}
       <mesh receiveShadow position={[0, 4, -6]}>
         <planeGeometry args={[12, 8]} />
         <meshStandardMaterial 
@@ -30,6 +24,7 @@ const Room: React.FC = () => {
         />
       </mesh>
       
+      {/* Front wall */}
       <mesh receiveShadow position={[0, 4, 6]}>
         <planeGeometry args={[12, 8]} />
         <meshStandardMaterial 
@@ -39,6 +34,7 @@ const Room: React.FC = () => {
         />
       </mesh>
       
+      {/* Left wall */}
       <mesh receiveShadow position={[-6, 4, 0]} rotation={[0, Math.PI / 2, 0]}>
         <planeGeometry args={[12, 8]} />
         <meshStandardMaterial 
@@ -48,6 +44,7 @@ const Room: React.FC = () => {
         />
       </mesh>
       
+      {/* Right wall */}
       <mesh receiveShadow position={[6, 4, 0]} rotation={[0, -Math.PI / 2, 0]}>
         <planeGeometry args={[12, 8]} />
         <meshStandardMaterial 
@@ -57,6 +54,7 @@ const Room: React.FC = () => {
         />
       </mesh>
       
+      {/* Ceiling */}
       <mesh receiveShadow position={[0, 8, 0]} rotation={[Math.PI / 2, 0, 0]}>
         <planeGeometry args={[12, 12]} />
         <meshStandardMaterial 
