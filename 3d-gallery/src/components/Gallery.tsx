40c27779b86482b6b2375182b3207d43e4396e91
Brea Miller <brea.m.miller@gmail.com>
2025-10-31 03:58:16 +0000

    test: add FirstPersonControls back

--- 3d-gallery/src/components/Gallery.tsx
+++ 3d-gallery/src/components/Gallery.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Lighting from './Lighting'
 import Room from './Room'
+import FirstPersonControls from './FirstPersonControls'
 
 const Gallery: React.FC = () => {
   return (
     <>
       <Lighting />
       <Room />
+      <FirstPersonControls />
     </>
   )
 }
