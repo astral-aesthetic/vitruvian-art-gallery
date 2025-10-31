2ee95c5a37672793bc8152027c7e0be3932af863
Brea Miller <brea.m.miller@gmail.com>
2025-10-31 01:31:23 +0000

    Re-enable FirstPersonControls

--- 3d-gallery/src/components/Gallery.tsx
+++ 3d-gallery/src/components/Gallery.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Lighting from './Lighting'
+import FirstPersonControls from './FirstPersonControls'
 
 const Gallery: React.FC = () => {
   try {
-    console.log('[Gallery] Rendering minimal scene - just lighting')
+    console.log('[Gallery] Rendering with FirstPersonControls enabled')
     return (
       <>
         <Lighting />
+        <FirstPersonControls />
         {/* DISABLED COMPONENTS - To be re-enabled:
           <Room />
           <Sky />
           <Paintings />
-          <FirstPersonControls />
         */}
       </>
     )
